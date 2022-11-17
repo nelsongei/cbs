e1854a34f419595fbb84dc8316134df2635b73ce
Nelson Sammy <psammy.ngei@gmail.com>
2022-11-17 09:24:20 +0300

    added member guarantor

---
+++ app/Http/Controllers/MemberGuarantorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemberGuarantorController extends Controller
+{
+    //
+    public function store()
+    {}
+}
---
+++ database/migrations/2022_11_17_061857_create_member_guarantors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberGuarantorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_guarantors', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('member_id')->unsigned();
+            $table->foreign('member_id')->references('id')->on('members')->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('organization_id')->unsigned();
+            $table->foreign('organization_id')->references('id')->on('organizations')->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('guarantor_id')->unsigned();
+            $table->foreign('guarantor_id')->references('id')->on('members')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('guarantee_percentage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_guarantors');
+    }
+}
