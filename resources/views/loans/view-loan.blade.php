56f9ee907a644f10b8fc003cf1b2ebfd46033b1b
Nelson Sammy <psammy.ngei@gmail.com>
2022-11-26 14:43:02 +0300

    loan application

--- resources/views/loans/view-loan.blade.php
+++ resources/views/loans/view-loan.blade.php
@@ -34,7 +34,7 @@
                                 <div class="col-sm-3">
                                     <div class="card">
                                         <div class="card-body text-center">
-                                            <img class="img-fluid img-rounded img-circle img-150"
+                                            <img class="img-fluid img-rounded img-circle img-100"
                                                  src="{{asset('images/payroll2.gif')}}" alt="img">
                                             <h4 class="text-c-blue">{{$loan->member->firstname.' '.$loan->member->lastname}}</h4>
                                             <h4 class="text-success">{{$loan->account_number}}</h4>
@@ -48,18 +48,89 @@
                                             <p class="text-muted">
                                                 {{$loan->loanType->name}}
                                             </p>
-                                            <hr>
-                                            <strong class="text-success">
-                                                <i class="fa fa-book mr-1"></i>Loan type
+                                            <strong class="text-info">
+                                                <i class="fa fa-book mr-1"></i>Loan Balance
                                             </strong>
                                             <p class="text-muted">
-                                                {{$loan->loanType->name}}
+                                                10000
                                             </p>
-                                            <hr>
+                                            <strong class="text-warning">
+                                                <i class="fa fa-book mr-1"></i>Date Disbursed
+                                            </strong>
+                                            <p class="text-muted">
+                                                11/11/2022
+                                            </p>
+                                            <strong class="text-danger">
+                                                <i class="fa fa-book mr-1"></i>Amount Disbursed
+                                            </strong>
+                                            <p class="text-muted">
+                                                10000
+                                            </p>
+                                            <strong class="text-c-green">
+                                                <i class="fa fa-book mr-1"></i>Principal Paid
+                                            </strong>
+                                            <p class="text-muted">
+                                                0
+                                            </p>
+                                            <strong class="text-c-orenge">
+                                                <i class="fa fa-book mr-1"></i>Amount Disbursed
+                                            </strong>
+                                            <p class="text-muted">
+                                                2000
+                                            </p>
+                                            <strong class="text-primary">
+                                                <i class="fa fa-book mr-1"></i>Principal Balance
+                                            </strong>
+                                            <p class="text-muted">
+                                                2000
+                                            </p>
+                                            <strong class="text-primary">
+                                                <i class="fa fa-book mr-1"></i>Loan Period
+                                            </strong>
+                                            <p class="text-muted">
+                                                3 Months
+                                            </p>
+                                            <strong class="text-pinterest">
+                                                <i class="fa fa-book mr-1"></i>Interest Rate
+                                            </strong>
+                                            <p class="text-muted">
+                                                3 %
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-9"></div>
+                                <div class="col-sm-9">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <ul class="nav nav-pills">
+                                                <li class="nav-item">
+                                                    <a href="#schedule" class="active nav-link"
+                                                       data-toggle="tab">Loan Schedule</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#transaction" class="nav-link"
+                                                       data-toggle="tab">Loan Transactions</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#more" class="nav-link"
+                                                       data-toggle="tab">More Information</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="#docs" class="nav-link"
+                                                       data-toggle="tab">Loan Documents</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="tab-content">
+                                                <div id="schedule" class="tab-pane"></div>
+                                                <div id="transaction" class="tab-pane"></div>
+                                                <div id="more" class="tab-pane"></div>
+                                                <div id="docs" class="tab-pane"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
