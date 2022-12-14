c2dbea77bfd7a981dd37f67c3eb682bd7ca6bd99
Nelson Sammy <psammy.ngei@gmail.com>
2022-12-14 05:34:01 +0300

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,16 +103,39 @@
                             <div class="col-xl-6 col-md-12">
                                 <div class="card">
                                     <div class="card-header">
+                                        <h3 class="title">Gender Count</h3>
                                     </div>
-                                    <div class="card-body" >
+                                    <div class="card-body" style="justify-content: center;display: flex;">
+                                        <canvas id="gender" height="400vw" width="400vw"></canvas>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-6 col-md-12">
                                 <div class="card">
                                     <div class="card-header">
+                                        Loan Released
                                     </div>
-                                    <div class="card-body" >
+                                    <div class="card-body" style="justify-content: center;display: flex;">
+                                        <canvas id="loanReleased" height="150px" width="400vw"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-md-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        Loan Collections
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="card-body" style="justify-content: center;display: flex;">
+                                            <canvas id="loanCollection" height="150px" width="400vw"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-md-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <canvas id="fullyPaid" height="150px" width="400vw"></canvas>
                                     </div>
                                 </div>
                             </div>
@@ -122,4 +145,135 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="{{asset('js/pureknob.js')}}" type="text/javascript"></script>
+    <script>
+        const ctx = document.getElementById('gender');
+
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Male', 'Female',],
+                datasets: [{
+                    label: '# Gender Count',
+                    data: [12, 19],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        display: false,
+                    }
+                }
+            }
+        });
+    </script>
+    <script>
+        const loanReleased = document.getElementById('loanReleased');
+
+        new Chart(loanReleased, {
+            type: 'line',
+            data: {
+                labels: ['January', 'Feb','March','April','May','June','July'],
+                datasets: [{
+                    label: '# Loans Released',
+                    data: [12000, 19000,5000,7000,18900,1820,10000],
+                    backgroundColor: [
+                        '#6dd144',
+                        // 'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        '#6dd144',
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+             //   responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                elements: {
+                    line: {
+                        tension: 0.5
+                    }
+                }
+            }
+        });
+    </script>
+    <script>
+        const loanCollection = document.getElementById('loanCollection');
+
+        new Chart(loanCollection, {
+            type: 'line',
+            data: {
+                labels: ['January', 'Feb','March','April','May','June','July'],
+                datasets: [{
+                    label: '# Loans Collection',
+                    data: [12000, 19000,5000,7000,18900,1820,10000],
+                    backgroundColor: [
+                        '#644ec5',
+                        // 'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        '#644ec5',
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+             //   responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                elements: {
+                    line: {
+                        tension: 0.5
+                    }
+                }
+            }
+        });
+    </script>
+    <script>
+        const fullyPaid = document.getElementById('fullyPaid');
+
+        new Chart(fullyPaid, {
+            type: 'line',
+            data: {
+                labels: ['January', 'Feb','March','April','May','June','July'],
+                datasets: [{
+                    label: '# Fully Paid',
+                    data: [12000, 19000,5000,7000,18900,1820,10000],
+                    backgroundColor: [
+                        '#644ec5',
+                        // 'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        '#644ec5',
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+             //   responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                elements: {
+                    line: {
+                        tension: 0.5
+                    }
+                }
+            }
+        });
+    </script>
 @endsection
