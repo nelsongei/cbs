cc612eee61c100d691903babf79912fe2000af2b
Nelson Sammy <psammy.ngei@gmail.com>
2023-01-18 13:13:03 +0300

    member import updated

--- app/Imports/MemberImport.php
+++ app/Imports/MemberImport.php
@@ -23,22 +23,3 @@ class MemberImport implements ToModel,WithStartRow
         return 2;
     }
 }
-
-        // $id = (count(Member::where('organization_id',Auth::user()->organization_id)->get())) + 1;
-        // return new Member([
-        //     'firstname' => $row[0],
-        //     'lastname' => $row[1],
-        //     'email' => $row[2],
-        //     'phone' => $row[3],
-        //     'title' => $row[4],
-        //     'address' => $row[5],
-        //     'id_no' => $row[6],
-        //     'branch_id' => 1,
-        //     'group_id' => 1,
-        //     'membership_no' => ('Lixnet -- ' . $id),
-        //     'dob' => $row[7],
-        //     'nationality' => $row[8],
-        //     'gender' => $row[9],
-        //     'organization_id' => Auth::user()->organization_id,
-        // ]);
-        //$id++;
