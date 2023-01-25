e06ce6792ebbb4fc061b2a640f3cbe9d88540d93
Nelson Sammy <psammy.ngei@gmail.com>
2023-01-25 11:49:34 +0300

    updates

--- app/Http/Controllers/LoanReportController.php
+++ app/Http/Controllers/LoanReportController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LoanApplication;
 use App\Models\LoanProduct;
+use App\Models\Organization;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -20,6 +23,14 @@ class LoanReportController extends Controller
     }
     public function download(Request $request)
     {
-        dd($request->all());
+        $organization = Organization::find(Auth::user()->organization_id);
+        if($request->period=='asatdate' && $request->format =='PDF'&& ($request->report !='listing' || $request->report !='arrears'))
+        {
+            $period = $request->date;
+            $loanProduct = LoanProduct::find($request->report);
+            $loans = LoanApplication::where('organization_id',Auth::user()->organization_id)->where('loan_product_id',$request->report)->get();
+            $pdf = Pdf::loadView('pdf.loanreports',compact('loans','organization','period','loanProduct'));
+            return $pdf->stream('loans.pdf');
+        }
     }
 }
